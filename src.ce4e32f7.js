7149ad3384acfa06c276000df974d50f49f0f041
AntonZenko <94699930+AntonZenko@users.noreply.github.com>
2022-02-21 21:59:14 +0000

    Deploying to gh-pages from @ AntonZenko/team-project-5@c44fade9339a5c19516be19c806208b531eb7645 🚀

--- src.ce4e32f7.js
+++ src.ce4e32f7.js
@@ -5,4 +5,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/script");
 },{"./sass/main.scss":"clu1","./js/script":"L4bL"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-project-5/src.5276abd9.js.map
+//# sourceMappingURL=/team-project-5/src.ce4e32f7.js.map
