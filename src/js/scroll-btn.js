0e7563ac36fb236ab8a64c19ed2fbde41328c4b1
AntonZenko <zenko_anton@ukr.net>
2022-02-22 22:12:52 +0200

    fix scroll-btn

--- src/js/scroll-btn.js
+++ src/js/scroll-btn.js
@@ -1,23 +1,23 @@
 
-      //кнопка прокрутки
-      // Получить кнопку
-    var mybutton = document.getElementById('myBtn');
+    //   //кнопка прокрутки
+    //   // Получить кнопку
+    // var mybutton = document.getElementById('myBtn');
 
-      // Когда пользователь прокручивает вниз 20px от верхней части документа, покажите кнопку
-    window.onscroll = function () {
-        scrollFunction();
-        };
+    //   // Когда пользователь прокручивает вниз 20px от верхней части документа, покажите кнопку
+    // window.onscroll = function () {
+    //     scrollFunction();
+    //     };
 
-    function scrollFunction() {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            mybutton.style.display = 'block';
-        } else {
-            mybutton.style.display = 'none';
-        }
-    }
+    // function scrollFunction() {
+    //     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    //         mybutton.style.display = 'block';
+    //     } else {
+    //         mybutton.style.display = 'none';
+    //     }
+    // }
 
-      // Когда пользователь нажимает на кнопку, прокрутите до верхней части документа
-    function topFunction() {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-    }
+    //   // Когда пользователь нажимает на кнопку, прокрутите до верхней части документа
+    // function topFunction() {
+    //     document.body.scrollTop = 0;
+    //     document.documentElement.scrollTop = 0;
+    // }
